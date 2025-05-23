be09697dc66641b123d855c1ebe2a5f912cc79c6
Jhonata Caetano <202402124731@alunos.estacio.br>
2025-05-22 22:36:42 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+// Cadastro.js
+console.log("O código está rodando!");
+
+// amimação do botão de login
+    document.addEventListener('DOMContentLoaded', function () {
+    const botao = document.getElementById('login-btn');
+  
+    botao.addEventListener('click', function () {
+      window.location.href = '../HTML/index.html'; // Troque pelo destino real
+    });
+  });
+  
