83948c26ea8be9abd1bec318111f04caa34e3323
Jhonata Caetano <caetanojhonata@hotmail.com>
2025-06-01 00:13:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-// Cadastro.js
-console.log("O código está rodando!");
 
-// amimação do botão de login
-    document.addEventListener('DOMContentLoaded', function () {
-    const botao = document.getElementById('login-btn');
-  
-    botao.addEventListener('click', function () {
+   botao.addEventListener('DOMContentLoaded', function () {
+       // chamando o botão pelo nome que ele tem no html
+       const botao = document.getElementById('login-btn');
+       botao.assEventListener('click', function () {
       window.location.href = 'index2.html'; // Troque pelo destino real
     });
-  });
-  
